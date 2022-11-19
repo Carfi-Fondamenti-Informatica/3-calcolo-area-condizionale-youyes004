c054494edf0591a7c9aeac3a70d8b025807f743a
Yousef El Sayed <115503073+youyes004@users.noreply.github.com>
2022-11-19 14:54:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-
 int main() {
-   return 0;
-}
+    float a,b;
+    int opzione=0;
+    cin>>a>>b>>opzione;
+    switch(opzione){
+        case 0:
+            cout<<(a*b)/2<<endl;
+            break;
+        case 1:
+            cout<<pow(a,2)<<endl;
+            break;
+        case 2:
+            cout<<a*b<<endl;
+            break;
+        default:
+            cout<<"opzione non valida"<<endl;
+    }
